528e10b90c642e684035a368d2e206dc0b98c8ca
coffee-cup <jakerunzer@gmail.com>
2014-05-25 20:53:07 -0700

    add skeleton project

---
+++ server.js
@@ -0,0 +1,27 @@
+// modules
+var experss  = require('express');
+var app      = express();
+var mongoose = require('mongoose');
+
+// configuration
+
+// config files
+var db = require('./config/db');
+
+var port = process.env.PORT || 9090;
+// mongoose.connect(db.url);
+
+app.configure(function () {
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+});
+
+// routes
+require('./app/routes')(app);
+
+// start app
+app.listen(port);
+console.log('Magic happens on prot ' + port);
+exports = module.exports = app;
