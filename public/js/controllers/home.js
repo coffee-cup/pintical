70058e1fce46b8220f4b02128d4eb6fb7387bd67
Ben Carroll <benjamincarroll93@gmail.com>
2014-06-01 17:20:55 -0700

    grid applied to home.html

--- public/js/controllers/home.js
+++ public/js/controllers/home.js
@@ -1,6 +1,14 @@
 angular.module('homeController.controller', [])
-  .controller('homeController', function($scope, $location) {
+  .controller('homeController', function($scope, $location, pageService) {
     $scope.findPage = function(name) {
       $location.path('/' + name);
+    }
+
+    $scope.allPages = function(){
+        pageService.allPages().success(function(pages) {
+        $scope.pages = pages;
+      }).error(function(err) {
+        $scope.err = err.message;
+      });
     }
   });
